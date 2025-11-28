7419d4d4c7c5006ab54225eb882addec41c5a51c
Max-Moreno <morenoaliaga@gmail.com>
2025-11-28 08:51:14 -0300

    deploy2

---
+++ main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.77.0"
+    }
+  }
+}
+
+provider "google" {
+  project     = var.project_id
+  region      = var.region
+
+  zone        = var.zone
+}
+
+module "instances" {
+
+  source     = "./modules/instances"
+
+}
